f00944697ae3cace1168931f6cf087d8c7edd7f1
Cyril Wibowo <z5478249@ad.unsw.edu.au>
2025-11-30 19:57:11 +1100

    added function to formate dates

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,3 +2,10 @@ export const formatCurrency = (amount: string): string => {
   const num = parseFloat(amount);
   return `$${num.toLocaleString('en-US')}`;
 }
+export const formatDate = (dateStr: string): string => {
+  const date = new Date(dateStr);
+  const d = String(date.getDate()).padStart(2, "0");
+  const m = String(date.getMonth() + 1).padStart(2, "0");
+  const y = date.getFullYear();
+  return `${d}/${m}/${y}`;
+}
