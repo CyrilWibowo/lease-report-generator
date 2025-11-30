dba0b0359d422aa18930a90f6d4ffa80aba15c9c
Cyril Wibowo <z5478249@ad.unsw.edu.au>
2025-11-30 20:02:00 +1100

    added function to get yearDiff

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,10 +2,15 @@ export const formatCurrency = (amount: string): string => {
   const num = parseFloat(amount);
   return `$${num.toLocaleString('en-US')}`;
 }
+
 export const formatDate = (dateStr: string): string => {
   const date = new Date(dateStr);
   const d = String(date.getDate()).padStart(2, "0");
   const m = String(date.getMonth() + 1).padStart(2, "0");
   const y = date.getFullYear();
   return `${d}/${m}/${y}`;
+}
+
+export const getYearDiff = (start: Date, end: Date): number => {
+  return Math.floor((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24 * 365))
 }
