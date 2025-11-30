2f82fd3c43f21846547202b284b486177bd2ce29
Cyril Wibowo <z5478249@ad.unsw.edu.au>
2025-11-30 19:47:57 +1100

    added different types of lease

--- src/types/Lease.ts
+++ src/types/Lease.ts
@@ -1,15 +1,32 @@
-export interface Lease {
+// types/Lease.ts
+export interface BaseLease {
   id: string;
-  type: 'Property' | 'Motor Vehicle';
-  propertyAddress?: string;
-  description?: string;
-  commencementDate: string;
-  expiryDate: string;
-  options: string;
-  originalAnnualRent: string;
+  lessor: string;
   rbaCpiRate: string;
-  fixedIncrementRate: string;
   borrowingRate: string;
   incrementMethods: { [year: number]: string };
   overrideAmounts: { [year: number]: string };
 }
+
+export interface PropertyLease extends BaseLease {
+  type: 'Property';
+  propertyAddress: string;
+  commencementDate: string;
+  expiryDate: string;
+  options: string;
+  annualRent: string;
+  fixedIncrementRate: string;
+}
+
+export interface MotorVehicleLease extends BaseLease {
+  type: 'Motor Vehicle';
+  entityName: string;
+  description: string;
+  vinSerialNo: string;
+  regoNo: string;
+  deliveryDate: string;
+  expiryDate: string;
+  annualRent: string;
+}
+
+export type Lease = PropertyLease | MotorVehicleLease;
