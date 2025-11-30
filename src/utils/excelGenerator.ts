dc34a50cfb2e8a83352909ab7c493e01d19cc5b0
Cyril Wibowo <z5478249@ad.unsw.edu.au>
2025-12-01 10:16:21 +1100

    generateExcel function now creates PV calculation sheet

--- src/utils/excelGenerator.ts
+++ src/utils/excelGenerator.ts
@@ -1,15 +1,13 @@
 import * as XLSX from 'xlsx';
 import { PropertyLease } from '../types/Lease';
-import { generatePaymentRows } from './excelHelper';
 import { generateLeasePayments } from './leasePaymentsSheetGenerator';
+import { generatePVCalculation } from './PVCalculationSheetGenerator';
 
 export const generateExcelFromLeases = (lease: PropertyLease) => {
   const workbook = XLSX.utils.book_new();
 
-  XLSX.utils.book_append_sheet(
-    workbook, generateLeasePayments(lease, generatePaymentRows(lease)),
-    "Lease Payements"
-  );
+  XLSX.utils.book_append_sheet(workbook, generateLeasePayments(lease), "Lease Payements");
+  XLSX.utils.book_append_sheet(workbook, generatePVCalculation(lease), "PV Calculation");
 
   XLSX.writeFile(workbook, `${lease.propertyAddress}.xlsx`);
 };
