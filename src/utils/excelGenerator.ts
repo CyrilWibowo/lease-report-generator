0b1806634d8820a4460c01b60def17d29f592305
Cyril Wibowo <z5478249@ad.unsw.edu.au>
2025-12-01 19:54:28 +1100

    updated PV sheet function to pass params

--- src/utils/excelGenerator.ts
+++ src/utils/excelGenerator.ts
@@ -2,12 +2,13 @@ import * as XLSX from 'xlsx';
 import { PropertyLease } from '../types/Lease';
 import { generateLeasePayments } from './leasePaymentsSheetGenerator';
 import { generatePVCalculation } from './PVCalculationSheetGenerator';
+import { XLSXGenerationParams } from '../components/ToXLSXModal';
 
-export const generateExcelFromLeases = (lease: PropertyLease) => {
+export const generateExcelFromLeases = (lease: PropertyLease, params: XLSXGenerationParams) => {
   const workbook = XLSX.utils.book_new();
 
   XLSX.utils.book_append_sheet(workbook, generateLeasePayments(lease), "Lease Payements");
-  XLSX.utils.book_append_sheet(workbook, generatePVCalculation(lease), "PV Calculation");
+  XLSX.utils.book_append_sheet(workbook, generatePVCalculation(lease, params), "PV Calculation");
 
   XLSX.writeFile(workbook, `${lease.propertyAddress}.xlsx`);
 };
