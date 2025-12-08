a177985db5dc660878f283a19963ec092551dede
Cyril Wibowo <cd.wibowo@gmail.com>
2025-12-08 12:17:51 +1100

    add TypeScript types for Electron API

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+
+import { Lease } from './types/Lease';
+
+declare global {
+  interface Window {
+    electronAPI?: {
+      loadLeases: () => Promise<Lease[]>;
+      saveLeases: (leases: Lease[]) => Promise<boolean>;
+    };
+  }
+}
