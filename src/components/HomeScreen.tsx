a9c5865f368183add04ee8f36d09e6f47c902292
Cyril Wibowo <cd.wibowo@gmail.com>
2025-12-10 10:04:00 +1100

    added icons to tools buttons

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import HomeWorkIcon from '@mui/icons-material/HomeWork';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import PaymentsIcon from '@mui/icons-material/Payments';
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import PercentIcon from '@mui/icons-material/Percent';
+import PaidIcon from '@mui/icons-material/Paid';
 import cwTechnicaLogo from '../assets/C&WTechnicaLogo.png';
 import './HomeScreen.css';
 
@@ -17,21 +23,27 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ onNavigateToLeases }) => {
         <div className="tool-buttons-grid">
           <button className="tool-button" onClick={onNavigateToLeases}>
             Leases
+            <HomeWorkIcon className="tool-button-icon" />
           </button>
           <button className="tool-button disabled" disabled>
             Fixed Assets
+            <AccountBalanceIcon className="tool-button-icon" />
           </button>
           <button className="tool-button disabled" disabled>
             Prepayments
+            <PaymentsIcon className="tool-button-icon" />
           </button>
           <button className="tool-button disabled" disabled>
             Bonds Register
+            <ReceiptLongIcon className="tool-button-icon" />
           </button>
           <button className="tool-button disabled" disabled>
             Royalty & Corp. Fee
+            <PercentIcon className="tool-button-icon" />
           </button>
           <button className="tool-button disabled" disabled>
             Dividend
+            <PaidIcon className="tool-button-icon" />
           </button>
         </div>
       </div>
